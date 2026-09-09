124c584e8ec1a76a784c6b7963bd4c2185f58b31
Archit <architbishnoi177@gmail.com>
2026-09-09 18:25:14 +0530

    Time: 0 ms (100%), Space: 11.9 MB (39.33%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+
+        string prefix = strs[0]; // start with the first string
+        for (int i = 1; i < strs.size(); i++) {
+            // shrink prefix until it matches the start of strs[i]
+            while (strs[i].find(prefix) != 0) {
+                prefix = prefix.substr(0, prefix.size() - 1);
+                if (prefix.empty()) return "";
+            }
+        }
+        return prefix;
+    }
+};
