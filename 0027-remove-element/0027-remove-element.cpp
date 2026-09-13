d0b447cf50f79327a7aa0f0526c5d7e58e66e427
Archit <architbishnoi177@gmail.com>
2026-09-13 19:53:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,6 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+              int i = 0; // pointer for placement        for (int j = 0; j < nums.size(); j++) {}
+    }
+};
